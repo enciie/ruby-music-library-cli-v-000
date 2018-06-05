b9b9ba326f9f70eb6951671a415bbbdb641b04a5
Nancy Jo <enciie@gmail.com>
2018-06-05 21:29:16 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,7 +1,5 @@
 class Artist
   extend Concerns::Findable #Take all of the methods in the Findable module and add them as class methods
-  extend Concerns::Persistable
-  include Concerns::Persistable
 
   attr_accessor :name
   attr_reader :songs
@@ -17,13 +15,13 @@ class Artist
     @@all
   end
 
-  # def save # Instance Method
-  #   @@all << self
-  # end
-  #
-  # def self.destroy_all #Helper Method
-  #   @@all.clear
-  # end
+  def save # Instance Method
+    @@all << self
+  end
+
+  def self.destroy_all #Class Helper Method
+    @@all.clear
+  end
 
   def self.create(name)
     self.new(name).tap {|a| a.save}
