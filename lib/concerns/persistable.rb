c714f3a1fdfdfa97c87bdda317ab2b20c75712b9
Nancy Jo <enciie@gmail.com>
2018-06-05 21:25:03 +0000

    automatically backed up by learn

--- lib/concerns/persistable.rb
+++ lib/concerns/persistable.rb
@@ -1,15 +1,9 @@
 module Concerns::Persistable
-
-  module InstanceMethods
-    def save #Instance Method
+  def save #Instance Method
       self.class.all << self
     end
-  end
 
-  module ClassMethods
     def destroy_all #Class Method
       self.all.clear
     end
-  end
-
 end
